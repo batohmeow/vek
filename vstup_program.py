1fa170024c3efe04342298d3f50e95f1a127df30
batohmeow <retardjeruzovej@gmail.com>
2025-01-21 15:53:46 +0100

    Update vstup_program.py

--- vstup_program.py
+++ vstup_program.py
@@ -1,13 +1,9 @@
-
-# Zeptáme se na věk
 vek = int(input("Kolik ti je let? "))
 
-# Pokud je věk 18, pokračujeme
 if vek == 18:
-    # Zeptáme se na jméno
+    
     jmeno = input("Jaké je tvoje jméno? ")
     
-    # Pokud jméno začíná na "A" (nebo "a"), povolíme vstup
     if jmeno.lower().startswith("a"):
         print("Vstup povolen")
     else:
